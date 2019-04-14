44e288db63feea8f24f35cfbf7f60366c04f7956
bananaisnotafruit <bananaisnotafruit69@gmail.com>
2019-04-14 13:28:23 +0530

    Partial support for branched compounds added.

--- iupac.cpp
+++ iupac.cpp
@@ -17,44 +17,54 @@ public:
 }y;
 
 class branch{
+public:
   int len;
   int loc;
 }z;
 
 int main(){
   char cp[20];
-  int len=0, i=0, dbi=0, tbi=0, bri=0, brlen=0;
+  int len=0, i=0, dbi=0, tbi=0, bri=0, brlen=0, temp=0;
 
   fgets(cp, 20, stdin);
 
-  for(i = 0; cp[i] != 0; i++){
+  for(i = 0; cp[i] != '\0'; i++){
+
     if(cp[i] == 'C'){
       len++;
     }
+
     if(cp[i] == '='){
       x.stat=1;
       x.loc=i;
     }
 
     if(cp[i] == '('){
-      z.loc = i+1;
-      for(bri=0; cp[bri]!=')', bri ++){
+      z.loc = i;
+      for(bri=i; cp[bri]!=')'; bri++){
         if(cp[bri] == 'C'){
           z.len++;
-
         }
       }
+      i=bri-1;
+      temp = i;
     }
-  }
 
-  cout<<"The length of branch is "<<z.len<<endl;
+  }
 
   if(x.loc > len/2)// Changes the position to the lowest possible
   {
     x.loc = len - x.loc;
+      if(z.loc) {z.loc = len - z.loc + 1;}
+  }
+  if(z.loc > len/2 && x.stat == 0){
+    z.loc = len - z.loc + 1;
   }
 
   //cout<<"The length of the compound is "<<len<<endl;
+
+  if(z.loc)cout<<z.loc<<"-"<<dict[z.len-1]<<"yl";
+
 
   cout<<dict[len-1]; // prints the base name
 
