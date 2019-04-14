9b110e2bd550e8181938125e64b878a680ccb3ae
bananaisnotafruit <bananaisnotafruit69@gmail.com>
2019-04-14 11:16:35 +0530

    Adding support for branched compounds

--- iupac.cpp
+++ iupac.cpp
@@ -16,13 +16,18 @@ public:
   int loc;
 }y;
 
+class branch{
+  int len;
+  int loc;
+}z;
+
 int main(){
   char cp[20];
-  int len=0, dbi=0, tbi=0;
+  int len=0, i=0, dbi=0, tbi=0, bri=0, brlen=0;
 
   fgets(cp, 20, stdin);
 
-  for(int i = 0; cp[i] != 0; i++){
+  for(i = 0; cp[i] != 0; i++){
     if(cp[i] == 'C'){
       len++;
     }
@@ -30,8 +35,19 @@ int main(){
       x.stat=1;
       x.loc=i;
     }
+
+    if(cp[i] == '('){
+      z.loc = i+1;
+      for(bri=0; cp[bri]!=')', bri ++){
+        if(cp[bri] == 'C'){
+          z.len++;
+
+        }
+      }
+    }
   }
 
+  cout<<"The length of branch is "<<z.len<<endl;
 
   if(x.loc > len/2)// Changes the position to the lowest possible
   {
